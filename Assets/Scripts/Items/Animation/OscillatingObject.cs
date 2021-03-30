6d64760874e5b2df3b6a000f740ed91c89736f61
PotatoesBasket <PotatoesBasket@hotmail.com>
2021-03-30 17:11:10 +1100

    Fixed bad animation script, misc folder reorganizing
    - Realised the part of the AnimateCollectable where it moves the object up and down was insanely wrong lol. Must've been tired when I wrote that. (...Like I'm not tired now)
    - Also, broke up the script into 2 parts (one for the up/down, one for spin, more reuseable)
    - Moved scripts around so I can actually find them

---
+++ Assets/Scripts/Items/Animation/OscillatingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Moves an object back and forth along it's y axis
+
+public class OscillatingObject : MonoBehaviour
+{
+    [SerializeField] float loopTime = 3.0f;
+    [SerializeField] float distance = 0.15f;
+
+    Vector3 origin = Vector3.zero;
+
+    private void Start()
+    {
+        origin = transform.position;
+        origin.y += distance; // offset so that objects in editor appear at the lowest point of the loop to avoid floating into floors
+    }
+
+    private void Update()
+    {
+        float y = distance * Mathf.Cos(2.0f * 3.14159f * Time.time / loopTime);
+
+        transform.position = new Vector3(origin.x, origin.y + y, origin.z);
+    }
+}
---
+++ Assets/Scripts/Items/Animation/SpinningObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningObject : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 50;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, spinSpeed * Time.deltaTime, 0));
+    }
+}
